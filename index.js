977ff97000be4b59841091678b1e6f37ca183aed
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-07 07:39:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bot=new Discord.Client();
 var id=0;
 var safemode=0;
 //login
-bot.login(process.env.token)
+bot.login(process.env.token);
 
-.then(bot.setPresence({game:{name:"We're poor little souls, who have lost all control, and we're forced to take that role",type:0}}));
+bot.setPresence({game:{name:"We're poor little souls, who have lost all control, and we're forced to take that role",type:0}});
 
 
 bot.on("message",function(message){
