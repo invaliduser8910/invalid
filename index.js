4d8901f429c16abc88703cce45854d0f12ed03ba
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-07 01:24:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.channels.get("648490031815589888").send("[Entering shelter...]");
 bot.channels.get("648490031815589888").send("**[Bot startup successful]**");
     
 bot.channels.get("648490031815589888").send("**[invalid-user8910 v1.1 by invalid-user#8910]**");
-bot.channels.get("648490031815589888").send("**[Basement now open for messages; join the party!]**");       
+bot.channels.get("648490031815589888").send("**[Basement now open for messages]**");       
 }    
     
     
