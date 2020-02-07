f7179cf5b4dec9632aaf2c33c9f48b42f37031b9
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-07 07:06:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var safemode=0;
 //login
 bot.login(process.env.token)
 
-.then(bot.user.setGame("We're poor little souls, who have lost all control, and we're forced to take that role"));
+.then(bot.user.setPresence({game:{name:"We're poor little souls, who have lost all control, and we're forced to take that role",status:"online"}}));
 
 bot.on("message",function(message){
  
