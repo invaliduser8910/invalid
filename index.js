f9db4ff4a131059a920157db8abb8c9c4d95d060
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-08 11:50:10 +0530

    bug fix

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if(message.content==='+ssso'){
 if(message.content==='+sssr'){
 id=0;
 bot.channels.get("648490031815589888").send("**[Admin/Mod reset the ID]**");
-bot.channels.get("674977831587020810").send("**[ID got reset]**);
+bot.channels.get("674977831587020810").send("**[ID got reset]**");
 }
 if(message.content==='+ssss'){
 resetBot();
@@ -58,7 +58,7 @@ bot.channels.get("648490031815589888").send("**[Bot startup successful]**");
     
 bot.channels.get("648490031815589888").send("**[invalid-user8910 v1.1 by invalid-user#8910]**");
 bot.channels.get("648490031815589888").send("**[Basement now open for messages]**");      
-bot.channels.get("674977831587020810").send("**[ID got reset"]**);
+bot.channels.get("674977831587020810").send("**[ID got reset]**");
 }    
     
     
