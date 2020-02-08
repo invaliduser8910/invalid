cab40919cdb9ed2c7e2f53f1a4acbf87dcd55900
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-08 17:24:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Discord=require('discord.js');
 const bot=new Discord.Client();
 var id=0;
 var safemode=0;
-//login
+
 bot.login(process.env.token);
-//Create status
+
 bot.on('ready',function(evt){
 bot.user.setPresence({game:{name:"We're poor little souls, who have lost all control, and we're forced to take that role"},status:'online'});
 }); 
@@ -14,7 +14,7 @@ bot.on("message",function(message){
  if(message.channel.type==="dm" && safemode==0)
 {
 id=id+1;
-bot.channels.get("674977831587020810").send("Message: ||"+message.content+"||\nAuthor: ||"+message.author+"|| ID: "+id); //logs channel
+bot.channels.get("674977831587020810").send("Message: ||"+message.content+"||\nAuthor: ||"+message.member.user.tag+"|| ID: "+id); //logs channel
 bot.channels.get("648490031815589888").send("𝗔𝗻𝗼𝗻𝘆𝗺𝗼𝘂𝘀: `"+message.content+"`\nID: `"+id+"`");
 }
 if(message.content==='+sssx'){
@@ -24,7 +24,7 @@ if(message.content==='+sssx'){
 if(message.content==='+ssso'){
     //login
     bot.login(process.env.token);
-    bot.channels.get("648490031815589888").send("**[Admin/Mod enabled the chat]**"); //
+    bot.channels.get("648490031815589888").send("**[Admin/Mod enabled the chat]**"); 
     safemode=0;
 }
 if(message.content==='+sssr'){
@@ -54,8 +54,7 @@ bot.destroy();
 bot.login(process.env.token);
 bot.channels.get("648490031815589888").send("**[Reset successful]**");
 bot.channels.get("648490031815589888").send("**[Bot is starting]**");  
-bot.channels.get("648490031815589888").send("**[Bot startup successful]**");
-    
+bot.channels.get("648490031815589888").send("**[Bot startup successful]**"); 
 bot.channels.get("648490031815589888").send("**[invalid-user8910 v1.1 by invalid-user#8910]**");
 bot.channels.get("648490031815589888").send("**[Basement now open for messages]**");      
 bot.channels.get("674977831587020810").send("**[ID got reset]**");
