be87bf93c80926d648614e9c73538d257d5f0895
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-09 20:37:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.channels.get("648490031815589888").send("**[Admin/Mod enabled the chat; Main
    if (message.content.indexOf(pref) !== 0) return;
 const args = message.content.slice(pref.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  if (command==="calc")
+  if (command==="calc" && args.length==2)
   var n1=parseInt(args[0],10);
   var n2=parseInt(args[1],10);
  var n3=n1+n2;
