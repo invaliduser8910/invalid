8a845ccef54edf98e457a610d1bcd12a01a1716c
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-11 06:33:58 -0800

    Update index.js

--- index.js
+++ index.js
@@ -74,14 +74,12 @@ message.channel.send("Hmm lets see...\n> "+n1+"+"+n2+"="+n3+"\n> "+n1+"-"+n2+"="
 else if (command==="rps" && args.length==0){
 
   rpsh=args[0];
- console.log("args length 0");
-    
+message.channel.send("index 0");    
     if(rpsh=="r"||"p"||"s") 
     
     {
      
-     console.log("arg is r or p or s");
-     
+message.channel.send("value r|p|s");     
     rpsb=Math.floor(Math.random() * 3);     
 if (rpsh=="r") rpsh=0; if (rpsh=="p") rpsh=1; if (rpsh=="s") rpsh=2;
     if (rpsb=="r") rpsb=0; if(rpsb=="p") rpsb=1; if(rpsb=="s") rpsb=2;
