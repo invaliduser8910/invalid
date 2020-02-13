01a1c63a1958e400aeed46b18b2b7e35a22e7e21
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-13 06:13:25 -0800

    Admin commands and spam bug fixed

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ if(command===null) {return;}
 if(message.channel.type=="dm" && safemode==0)
 {
 id=id+1;
-bot.channels.get("674977831587020810").send("Message: ||"+message.content+"||\nAuthor: ||"+message.author.tag+"|| ID: "+id); //logs channel
-bot.channels.get("648490031815589888").send("𝗔𝗻𝗼𝗻𝘆𝗺𝗼𝘂𝘀: `"+message.content+"`\nID: `"+id+"`");
+bot.channels.get("674977831587020810").send("Message: ||"+message.content+"||\nAuthor: ||"+message.author.tag+"|| ID: "+id); //log channel
+bot.channels.get("648490031815589888").send("𝗔𝗻𝗼𝗻𝘆𝗺𝗼𝘂𝘀: `"+message.content+"`\nID: `"+id+"`"); //original channel
 return;  
-}//else if(message.channel.type!=="dm"){
+}
 
 const adminrole=message.guild.roles.find(r=>r.name==="ADMIN"); 
 const modrole=message.guild.roles.find(r=>r.name==="MODERATOR");   
@@ -93,7 +93,7 @@ switch(command)
   default: message.channel.send("Invalid command. Do #help for list of valid commands."); return;                     
 }
   
-//}
+
   
 function isNumeric(num){ return !isNaN(num); } 
  
