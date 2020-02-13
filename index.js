791abe92821e8310fa3a4e98fc647019b8567a67
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-13 06:19:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ switch(command)
                 else {return;} //if arguments are invalid 
                 break;
               }
-  default: message.channel.send("Invalid command. Do #help for list of valid commands."); return;                     
+  //default: message.channel.send("Invalid command. Do #help for list of valid commands."); return;                     
 }
   
 
