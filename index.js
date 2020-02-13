088a982553b32fbecb880fb0af84f6b8298e6d5c
invaliduser8910 <60749849+invaliduser8910@users.noreply.github.com>
2020-02-13 06:52:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ id=id+1;
 bot.channels.get("674977831587020810").send("Message: ||"+message.content+"||\nAuthor: ||"+message.author.tag+"|| ID: "+id); //log channel
 bot.channels.get("648490031815589888").send("𝗔𝗻𝗼𝗻𝘆𝗺𝗼𝘂𝘀: `"+message.content+"`\nID: `"+id+"`"); //original channel
 return;  
-}else{
+}else if(message.channel.type === 'guild'){
 
 const adminrole=message.guild.roles.find(r=>r.name==="ADMIN"); 
 const modrole=message.guild.roles.find(r=>r.name==="MODERATOR");   
